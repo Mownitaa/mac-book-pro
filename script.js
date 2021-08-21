ca349055b9f73e7d8b7c90a308b9e74938d760d6
Mownitaa <mabiamownita@gmail.com>
2021-08-21 09:46:39 +0600

    price added

--- script.js
+++ script.js
@@ -1,34 +1,77 @@
+
+// setting the value for each options of memory,storage and delivery process
+function addZero(value) {
+    const cost = document.getElementById(value + '-cost');
+    cost.innerText = 0;
+}
+function add180(value) {
+    const cost = document.getElementById(value + '-cost');
+    cost.innerText = 180;
+}
+// function add(price) {
+//     const addWithTotal = document.getElementById('total-price');
+//     addWithTotal.innerText = 1299 + price;
+// }
+// function addTotal(num) {
+//     const addWithTotalP = document.getElementById('total');
+//     addWithTotalP.innerText = 1299 + num;
+// }
 document.getElementById('8gb-btn').addEventListener('click', function () {
-    const memoryCost = document.getElementById('memory-cost');
-    memoryCost.innerText = 0;
+    addZero('memory');
+    // add(0);
+    // addTotal(0);
 });
 document.getElementById('16gb-btn').addEventListener('click', function () {
-    const memoryCost = document.getElementById('memory-cost');
-    memoryCost.innerText = 180;
+    add180('memory');
+    // add(180);
+    // addTotal(180);
 });
-
-
-
 document.getElementById('256gb-ssd-btn').addEventListener('click', function () {
-    const memoryCost = document.getElementById('storage-cost');
-    memoryCost.innerText = 0;
+    addZero('storage');
+    // add(0);
+    // addTotal(0);
 });
 document.getElementById('512gb-ssd-btn').addEventListener('click', function () {
     const memoryCost = document.getElementById('storage-cost');
     memoryCost.innerText = 100;
+    // add(100);
+    // addTotal(100);
 });
 document.getElementById('1tb-ssd-btn').addEventListener('click', function () {
-    const memoryCost = document.getElementById('storage-cost');
-    memoryCost.innerText = 180;
+    add180('storage');
+    // add(180);
+    // addTotal(180);
 });
-
-
-
 document.getElementById('free-delivery-btn').addEventListener('click', function () {
-    const memoryCost = document.getElementById('delivery-cost');
-    memoryCost.innerText = 0;
+    addZero('delivery');
+    // add(0);
+    // addTotal(0);
 });
 document.getElementById('paid-delivery-btn').addEventListener('click', function () {
     const memoryCost = document.getElementById('delivery-cost');
     memoryCost.innerText = 20;
+    // add(20);
+    // addTotal(20);
 });
+
+
+
+document.getElementById('options').addEventListener('click', function (event) {
+    // console.log('any key clicked');
+    console.log(event.target.value);
+    // const calcInput = document.getElementById('total-price');
+    // const previousNum = calcInput.value;
+    // const newNumber = 1299 + paeseInt(previousNum) + parseInt(option);
+    // calcInput.value = newNumber;
+})
+
+// document.getElementById('apply-btn').addEventListener('click', function () {
+
+    // const inputValue = document.getElementById('input-field').value;
+    // console.log(inputValue);
+    // // if (inputValue == 'stevekaku') {
+    // //     const totalPriceValue = document.getElementById('total').innerText - 0.2;
+    // //     return totalPriceValue;
+    // // }
+    // inputValue = '';
+// })
